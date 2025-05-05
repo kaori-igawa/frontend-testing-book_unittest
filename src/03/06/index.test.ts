45e93ce9332d9d5ab0b685f0366ec4ba8fa55544
kaori-igawa <kaori0v0igawa@gmail.com>
2025-05-05 19:22:50 +0900

    [3-6] 用途別のマッチャー

---
+++ src/03/06/index.test.ts
@@ -0,0 +1,138 @@
+describe('真偽値の検証', () => {
+  test('「真の値」の検証', () => {
+    expect(1).toBeTruthy();
+    expect('1').toBeTruthy();
+    expect(true).toBeTruthy();
+    expect(0).not.toBeTruthy();
+    expect('').not.toBeTruthy();
+    expect(false).not.toBeTruthy();
+  });
+  test('「偽の値」の検証', () => {
+    expect(0).toBeFalsy();
+    expect('').toBeFalsy();
+    expect(false).toBeFalsy();
+    expect(1).not.toBeFalsy();
+    expect('1').not.toBeFalsy();
+    expect(true).not.toBeFalsy();
+  });
+  test('「null, undefined」の検証', () => {
+    expect(null).toBeFalsy();
+    expect(undefined).toBeFalsy();
+    expect(null).toBeNull();
+    expect(undefined).toBeUndefined();
+    expect(undefined).not.toBeDefined();
+  })
+});
+
+describe('数値の検証', () => {
+  const value = 2 + 2;
+  test('検証値 は 期待値 と等しい', () => {
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+  })
+  test('検証値 は 期待値 より大きい', () => {
+    expect(value).toBeGreaterThan(3); // 4 > 3
+    expect(value).toBeGreaterThanOrEqual(4); // 4 >= 4
+  });
+  test('検証値 は 期待値 より小さい', () => {
+    expect(value).toBeLessThan(5); // 4 < 5
+    expect(value).toBeLessThanOrEqual(4); // 4 <= 4
+  });
+  test('小数計算は正確ではない', () => {
+    expect(0.1 + 0.2).not.toBe(0.3); // 検証値が0.30000000000000004になる
+  });
+  test('小数計算の指定桁までを比較する', () => {
+    expect(0.1 + 0.2).toBeCloseTo(0.3); // デフォルトは小数点以下2桁
+    expect(0.1 + 0.2).toBeCloseTo(0.3, 15);
+    expect(0.1 + 0.2).not.toBeCloseTo(0.3, 16);
+  });
+});
+
+describe('文字列の検証', () => {
+  const str = 'こんにちは世界';
+  const obj = { status: 200, message: str};
+  test('検証値 は 期待値 と等しい', () => {
+    expect(str).toBe('こんにちは世界');
+    expect(str).toEqual('こんにちは世界');
+  });
+  test('toHaveLength', () => {
+    // 検証値の文字列の長さを検証
+    expect(str).toHaveLength(7);
+    expect(str).not.toHaveLength(8);
+  });
+  test('toContain', () => {
+    // 検証値の文字列に期待値が含まれているかどうかを検証
+    expect(str).toContain('世界');
+    expect(str).not.toContain('さようなら');
+  });
+  test('toMatch', () => {
+    // 検証値の文字列が正規表現とマッチするかどうか検証
+    expect(str).toMatch(/世界/);
+    expect(str).not.toMatch(/さようなら/);
+  });
+  test('stringContaining', () => {
+    // 検証値のオブジェクトに含まれる文字列に期待値の文字列が含まれているかどうか検証
+    expect(obj).toEqual({
+      status: 200,
+      message: expect.stringContaining('世界'),
+    });
+  });
+  test('stringMatching', () => {
+    // 検証値のオブジェクトに含まれる文字列に期待値の正規表現とマッチするかどうか検証
+    expect(obj).toEqual({
+      status: 200,
+      message: expect.stringMatching(/世界/),
+    });
+  });
+});
+
+describe('配列の検証', () => {
+  describe('プリミティブ配列', () => {
+    const tags = ['Jest', 'Storybook', 'Playwright', 'React', 'Next.js'];
+    test('toContain', () => {
+      expect(tags).toContain('Jest'); // 配列に期待値が含まれているかどうか検証
+      expect(tags).toHaveLength(5); // 配列の長さが期待値とおなじかどうか検証
+    });
+  });
+  describe('オブジェクト配列', () => {
+    const article1 = { author: 'taro', title: 'Testing Next.js' };
+    const article2 = { author: 'jiro', title: 'Storybook play function' };
+    const article3 = { author: 'hanako', title: 'Visual Regression Testing ' };
+    const articles = [article1, article2, article3];
+    test('toContainEqual', () => {
+      expect(articles).toContainEqual(article1); // 配列に期待値のオブジェクトが含まれているかどうか検証
+    });
+    test('arrayContaining', () => {
+      expect(articles).toEqual(expect.arrayContaining([article1, article3])); // 配列に期待値のオブジェクトがすべて含まれているかどうか検証
+    });
+  });
+});
+
+describe('オブジェクトの検証', () => {
+  const author = { name: 'taroyamada', age: 38 };
+  const article = {
+    title: 'Testing with Jest',
+    author,
+  };
+  test('toMatchObject', () => {
+    expect(author).toMatchObject({ name: 'taroyamada', age: 38}); // オブジェクトに期待値のオブジェクトが一致するかどうか検証
+    expect(author).toMatchObject({ name: 'taroyamada' }); // オブジェクトに期待値のオブジェクトが一致するかどうか検証
+    expect(author).not.toMatchObject({ gender: 'man'}); // オブジェクトに期待値のオブジェクトが一致しないかどうか検証
+  });
+  test('toHaveProperty', () => {
+    // オブジェクトに期待値のプロパティがあるかどうか検証
+    expect(author).toHaveProperty('name');
+    expect(author).toHaveProperty('age');
+  });
+  test('objectContaining', () => {
+    // オブジェクトに含まれるオブジェクトを検証
+    expect(article).toEqual({
+      title: 'Testing with Jest',
+      author: expect.objectContaining({ name: 'taroyamada'}),
+    });
+    expect(article).toEqual({
+      title: 'Testing with Jest',
+      author: expect.not.objectContaining({ gender: 'man'}),
+    });
+  });
+});
