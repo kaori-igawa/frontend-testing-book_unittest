ffc1145a2eb6aeebd575a8a65d79f1d92954fe6e
kaori-igawa <kaori0v0igawa@gmail.com>
2025-05-10 17:53:42 +0900

    [5-4] アイテム一覧UIコンポーネントテスト

--- src/05/04/ArticleList.test.tsx
+++ src/05/04/ArticleList.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import { ArticleList } from './ArticleList';
 import { items } from './fixture';
 
@@ -14,4 +14,27 @@ test('一覧が表示される', () => {
   render(<ArticleList items={items} />);
   const list = screen.getByRole('list');
   expect(list).toBeInTheDocument();
-})
+});
+
+test('一覧が表示されて、itemsの数だけ一覧表示される', () => {
+  render(<ArticleList items={items} />);
+  const list = screen.getByRole('list');
+  expect(list).toBeInTheDocument();
+  // within ⇛ 指定のノードに含まれている要素を検証できる
+  expect(within(list).getAllByRole('listitem')).toHaveLength(3);
+});
+
+test('一覧アイテムが空のとき「投稿記事がありません」が表示される', () => {
+  // 空配列を与えて、一覧表示がない状態を再現する
+  render(<ArticleList items={[]} />)
+  // 存在しないと予測される要素の取得を試みる
+  // queryByRole ⇛ queryBy接頭辞のAPIはエラー発生でテストが中断しない
+  const list = screen.queryByRole('list');
+  // listが存在しないか検証
+  expect(list).not.toBeInTheDocument();
+  // listがnullであることを検証する
+  // expect(list).toBeNull();
+  // 「投稿記事がありません」が表示されていることを確認
+  expect(screen.getByText('投稿記事がありません')).toBeInTheDocument();
+});
+
---
+++ src/05/04/ArticleListItem.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { ArticleListItem, ItemProps } from './ArticleListItem';
+
+const item: ItemProps = {
+  id: "howto-testing-with-typescript",
+  title: "TypeScript を使ったテストの書き方",
+  body: "テストを書く時、TypeScript を使うことで、テストの保守性が向上します…",  
+}
+
+test('IDに紐づいたリンクが表示される', () => {
+  render(<ArticleListItem {...item} />);
+  // toHaveAttribute ⇛ 要素の属性を検証するマッチャー
+  expect(screen.getByRole('link', {name: 'もっと見る'})).toHaveAttribute('href', '/articles/howto-testing-with-typescript');
+});
