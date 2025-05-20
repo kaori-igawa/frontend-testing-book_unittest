8daefde971fcf3224298ed001ee648c50b601910
kaori-igawa <kaori0v0igawa@gmail.com>
2025-05-20 23:35:56 +0900

    [5-8] UIコンポーネントのスナップショットテスト 05/06まで

---
+++ src/05/06/ContactNumber.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import { ContactNumber } from './ContactNumber'
+
+describe('連絡先', () => {
+  test('タイトル', () => {
+    render(<ContactNumber />);
+    // expect(screen.getByRole('group', {name: '連絡先'})).toBeInTheDocument();
+    expect(screen.getByText('連絡先')).toBeInTheDocument();
+  });
+  test('電話番号', () => {
+    render(<ContactNumber />);
+    expect(screen.getByLabelText('電話番号')).toBeInTheDocument();
+  });
+  test('お名前', () => {
+    render(<ContactNumber />);
+    expect(screen.getByLabelText('お名前')).toBeInTheDocument();
+  });
+});
