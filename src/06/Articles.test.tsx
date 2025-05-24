aa20ea3af27885da0a1e0f59108f525859f9ba32
kaori-igawa <kaori0v0igawa@gmail.com>
2025-05-24 15:43:56 +0900

    [6-2] UIコンポーネントのテストカバレッジ

---
+++ src/06/Articles.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { Articles } from './Articles'
+
+
+describe('Articles test', () => {
+  test('読込中の場合「..loading」が表示される', () => {
+    render(<Articles items={[]} isLoading={true} />);
+    expect(screen.getByText('...loading')).toBeInTheDocument();
+  });
+  
+  test('一覧要素が空の場合「投稿記事がありません」が表示される', () => {
+    render(<Articles items={[]} isLoading={false} />);
+    expect(screen.getByText('投稿記事がありません')).toBeInTheDocument();
+  });
+  
+  test('一覧要素がある場合、一覧が表示される', () => {
+    const items = [
+      { id: 1, title: "Testing Next.js" },
+      { id: 2, title: "Storybook play function" },
+      { id: 3, title: "Visual Regression Testing " },
+    ];
+    render(<Articles items={items} isLoading={false} />);
+    expect(screen.getByRole('list')).toBeInTheDocument();
+  });
+});
